547792263cf732692c9401574745656e39daf0f2
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-12-13 10:02:13 +0300

    add: add project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-red-500">
+       <p>Zara blog landing page</p>
+    </main>
+  )
+}
